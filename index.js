2e68567693810e7e5262d5e7141f38e7ef1b9fe2
whimsical-navigator-7194 <jonesandjay123@yahoo.com.tw>
2019-02-21 01:23:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function takeANumber(katzDeliLine, newName){
-  
-  var position = fruits.indexOf(newName);
+  var postion = katzDeliLine.length + 1;
+  katzDeliLine.push(newName);
+  //var position = fruits.indexOf(newName);
   var message = "Welcome, "+ newName + ". You are number "+position+" in line.";
   return message;
 }
